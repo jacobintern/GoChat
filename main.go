387c3c678a70bf57dd06a7c765496dc0a60fa457
jacob <sles4511@gmail.com>
2021-01-30 00:00:48 +0800

    first golang web hello world

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello word")
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
