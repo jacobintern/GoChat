283bb6ef903b139ca83d35f0d588d50e5830c66d
jacob <sles4511@gmail.com>
2021-01-30 19:25:30 +0800

    create login page

--- main.go
+++ main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello word")
+func homePage(h http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(h, "Hello word")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	switch r.Method {
+	case "GET":
+		t, _ := template.ParseFiles("login.jacob")
+		log.Println(t.Execute(w, nil))
+		break
+	case "POST":
+		fmt.Println("username:", r.Form["acc"])
+		fmt.Println("password:", r.Form["pswd"])
+	}
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)
+	go http.HandleFunc("/", homePage)
+	go http.HandleFunc("/login", login)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
