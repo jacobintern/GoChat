b8a1829b93b675decc178c85ee799b8f1984ab3a
jacob <sles4511@gmail.com>
2021-01-31 11:41:29 +0800

    add crud and register page

--- main.go
+++ main.go
@@ -12,13 +12,35 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func loginPage(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("./views/login.html"))
-	tmpl.Execute(w, nil)
+	switch r.Method {
+	case "GET":
+		tmpl := template.Must(template.ParseFiles("./views/login.html"))
+		tmpl.Execute(w, nil)
+		break
+	case "POST":
+		r.ParseForm()
+		fmt.Println("username:", r.Form["acc"])
+		fmt.Println("password:", r.Form["pswd"])
+		break
+	case "PUT":
+		fmt.Fprintf(w, "put")
+		break
+	case "Delete":
+		fmt.Fprintf(w, "delete")
+		break
+	default:
+		return
+	}
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "register")
 }
 
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/login", loginPage)
+	http.HandleFunc("/register", register)
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
