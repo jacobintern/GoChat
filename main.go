e8cd32b1f55dd4f531b9ef66336b2d4f6b6c5ae6
jacob <sles4511@gmail.com>
2021-02-01 18:10:11 +0800

    add mongodb valid user and page(chatroom,register)

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"text/template"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func mongoDBcontext(dbName string, collectionName string) *mongo.Collection {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
+		"mongodb+srv://j_dev:zHYJQ2jc7UAqHThV@jdev.y4x5s.gcp.mongodb.net/"+dbName+"?retryWrites=true&w=majority",
+	))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client.Database(dbName).Collection(collectionName)
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello jacob")
@@ -19,8 +37,45 @@ func loginPage(w http.ResponseWriter, r *http.Request) {
 		break
 	case "POST":
 		r.ParseForm()
-		fmt.Println("username:", r.Form["acc"])
-		fmt.Println("password:", r.Form["pswd"])
+		acc := r.FormValue("acc")
+		pswd := r.FormValue("pswd")
+		if validUser(acc, pswd) {
+			http.Redirect(w, r, "/chatroom", http.StatusSeeOther)
+		} else {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+		}
+		break
+	case "PUT":
+		fmt.Fprintf(w, "put")
+		break
+	case "Delete":
+		fmt.Fprintf(w, "delete")
+		break
+	default:
+		return
+	}
+	return
+}
+
+func validUser(acc string, pswd string) bool {
+	collection := mongoDBcontext("chat_db", "chat_acc")
+	filter := bson.M{"acc": acc, "pswd": pswd}
+	// data, err := collection.Find(context.Background(), filter)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	data := collection.FindOne(context.Background(), filter)
+	return data.Err() != mongo.ErrNoDocuments
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		tmpl := template.Must(template.ParseFiles("./views/register.html"))
+		tmpl.Execute(w, nil)
+		break
+	case "POST":
+		fmt.Fprintf(w, "post")
 		break
 	case "PUT":
 		fmt.Fprintf(w, "put")
@@ -33,14 +88,36 @@ func loginPage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func register(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "register")
+func chatRoom(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		tmpl := template.Must(template.ParseFiles("./views/chatroom.html"))
+		tmpl.Execute(w, nil)
+		break
+	case "POST":
+		fmt.Fprintf(w, "post")
+		break
+	case "PUT":
+		fmt.Fprintf(w, "put")
+		break
+	case "Delete":
+		fmt.Fprintf(w, "delete")
+		break
+	default:
+		return
+	}
 }
 
 func main() {
+	// page
 	http.HandleFunc("/", home)
 	http.HandleFunc("/login", loginPage)
 	http.HandleFunc("/register", register)
+	http.HandleFunc("/chatroom", chatRoom)
+
+	// static
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
