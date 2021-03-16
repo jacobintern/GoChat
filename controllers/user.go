cdd6d14aaa774baadafdac5940c670cef2ae13b2
jacob <sles4511@gmail.com>
2021-03-16 17:34:00 +0800

    add cookie

--- controllers/user.go
+++ controllers/user.go
@@ -15,12 +15,26 @@ func GetUsers(w http.ResponseWriter, req *http.Request) {
 
 	if err != nil {
 		fmt.Println(err.Error())
-	} else {
-		fmt.Fprint(w, string(r))
 	}
+	fmt.Fprint(w, string(r))
+}
+
+// GetUsrCookies is
+func GetUsrCookies(w http.ResponseWriter, req *http.Request) {
+	r, err := json.Marshal(req.Cookies())
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Fprint(w, string(r))
 }
 
 // UserAPI is
 func UserAPI() {
 	http.HandleFunc("/api/GetUserList", GetUsers)
+}
+
+// GetCookies is
+func GetCookies() {
+	http.HandleFunc("/api/GetCookies", GetUsrCookies)
 }
